3372d487b06443e4b086dcc18a0f365d088fbf96
Yihong Zhang <yz489@cs.washington.edu>
2025-06-09 13:44:16 -0700

    migrate to the newer backend

--- src/rational.rs
+++ src/rational.rs
@@ -1,18 +1,12 @@
-use egglog::ast::Span;
-use egglog::ast::Symbol;
-use egglog::sort::F;
-use egglog::sort::{ExecutionState, Primitives};
+use egglog::sort::{Primitives, F};
 use num::integer::Roots;
 use num::rational::Rational64;
 use num::traits::{CheckedAdd, CheckedDiv, CheckedMul, CheckedSub, One, Signed, ToPrimitive, Zero};
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-pub struct R(Rational64);
+pub type R = egglog::Boxed<Rational64>;
 use crate::ast::Literal;
 
 use super::*;
-
-static RATIONAL_SORT_NAME: &str = "Rational";
 
 #[derive(Debug)]
 pub struct RationalSort;
@@ -20,43 +14,43 @@ pub struct RationalSort;
 impl LeafSort for RationalSort {
     type Leaf = R;
     fn name(&self) -> &str {
-        RATIONAL_SORT_NAME
+        "Rational"
     }
 
     #[rustfmt::skip]
     fn register_primitives(&self, eg: &mut EGraph) {
         // TODO we can't have primitives take borrows just yet, since it
         // requires returning a reference to the locked sort
-        add_primitive!(eg, "+" = |a: R, b: R| -?> R { a.0.checked_add(&b.0).map(R) });
-        add_primitive!(eg, "-" = |a: R, b: R| -?> R { a.0.checked_sub(&b.0).map(R) });
-        add_primitive!(eg, "*" = |a: R, b: R| -?> R { a.0.checked_mul(&b.0).map(R) });
-        add_primitive!(eg, "/" = |a: R, b: R| -?> R { a.0.checked_div(&b.0).map(R) });
+        add_primitive!(eg, "+" = |a: R, b: R| -?> R { a.0.checked_add(&b.0).map(R::new) });
+        add_primitive!(eg, "-" = |a: R, b: R| -?> R { a.0.checked_sub(&b.0).map(R::new) });
+        add_primitive!(eg, "*" = |a: R, b: R| -?> R { a.0.checked_mul(&b.0).map(R::new) });
+        add_primitive!(eg, "/" = |a: R, b: R| -?> R { a.0.checked_div(&b.0).map(R::new) });
 
-        add_primitive!(eg, "min" = |a: R, b: R| -> R { R(a.0.min(b.0)) });
-        add_primitive!(eg, "max" = |a: R, b: R| -> R { R(a.0.max(b.0)) });
-        add_primitive!(eg, "neg" = |a: R| -> R { R(-a.0) });
-        add_primitive!(eg, "abs" = |a: R| -> R { R(a.0.abs()) });
-        add_primitive!(eg, "floor" = |a: R| -> R { R(a.0.floor()) });
-        add_primitive!(eg, "ceil" = |a: R| -> R { R(a.0.ceil()) });
-        add_primitive!(eg, "round" = |a: R| -> R { R(a.0.round()) });
-        add_primitive!(eg, "rational" = |a: i64, b: i64| -> R { R(Rational64::new(a, b)) });
+        add_primitive!(eg, "min" = |a: R, b: R| -> R { R::new(a.0.min(b.0)) });
+        add_primitive!(eg, "max" = |a: R, b: R| -> R { R::new(a.0.max(b.0)) });
+        add_primitive!(eg, "neg" = |a: R| -> R { R::new(-a.0) });
+        add_primitive!(eg, "abs" = |a: R| -> R { R::new(a.0.abs()) });
+        add_primitive!(eg, "floor" = |a: R| -> R { R::new(a.0.floor()) });
+        add_primitive!(eg, "ceil" = |a: R| -> R { R::new(a.0.ceil()) });
+        add_primitive!(eg, "round" = |a: R| -> R { R::new(a.0.round()) });
+        add_primitive!(eg, "rational" = |a: i64, b: i64| -> R { R::new(Rational64::new(a, b)) });
         add_primitive!(eg, "numer" = |a: R| -> i64 { *a.0.numer() });
         add_primitive!(eg, "denom" = |a: R| -> i64 { *a.0.denom() });
 
-        add_primitive!(eg, "to-f64" = |a: R| -> F { egglog::sort::OrderedFloat(a.0.to_f64().unwrap()) });
+        add_primitive!(eg, "to-f64" = |a: R| -> F { F::new(egglog::sort::OrderedFloat(a.0.to_f64().unwrap())) });
 
         add_primitive!(eg, "pow" = |a: R, b: R| -?> R {
             if a.0.is_zero() {
                 if b.0.is_positive() {
-                    Some(R(Rational64::zero()))
+                    Some(R::new(Rational64::zero()))
                 } else {
                     None
                 }
             } else if b.0.is_zero() {
-                Some(R(Rational64::one()))
+                Some(R::new(Rational64::one()))
             } else if let Some(b) = b.0.to_i64() {
                 if let Ok(b) = usize::try_from(b) {
-                    num::traits::checked_pow(a.0, b).map(R)
+                    num::traits::checked_pow(a.0, b).map(R::new)
                 } else {
                     // TODO handle negative powers
                     None
@@ -67,7 +61,7 @@ impl LeafSort for RationalSort {
         });
         add_primitive!(eg, "log" = |a: R| -?> R {
             if a.0.is_one() {
-                Some(R(Rational64::zero()))
+                Some(R::new(Rational64::zero()))
             } else {
                 todo!()
             }
@@ -78,7 +72,7 @@ impl LeafSort for RationalSort {
                 let s2 = a.0.denom().sqrt();
                 let is_perfect = &(s1 * s1) == a.0.numer() && &(s2 * s2) == a.0.denom();
                 if is_perfect {
-                    Some(R(Rational64::new(s1, s2)))
+                    Some(R::new(Rational64::new(s1, s2)))
                 } else {
                     None
                 }
@@ -88,7 +82,7 @@ impl LeafSort for RationalSort {
         });
         add_primitive!(eg, "cbrt" = |a: R| -?> R {
             if a.0.is_one() {
-                Some(R(Rational64::one()))
+                Some(R::new(Rational64::one()))
             } else {
                 todo!()
             }
@@ -106,7 +100,7 @@ impl LeafSort for RationalSort {
         value: Value,
         termdag: &mut TermDag,
     ) -> Term {
-        let rat = primitives.unwrap_ref::<R>(value);
+        let rat = primitives.unwrap::<R>(value);
 
         let numer = rat.0.numer();
         let denom = rat.0.denom();
