7735de78dbb6524f0599a29f2e7a2cd04e5fc43f
Yihong Zhang <yz489@cs.washington.edu>
2025-06-11 14:33:51 -0700

    Remove run-schedule*

--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,8 @@ pub fn new_experimental_egraph() -> EGraph {
     egraph.parser.add_command_macro(Arc::new(For));
     egraph.parser.add_command_macro(Arc::new(WithRuleset));
 
-    egraph.parser.add_command_macro(Arc::new(Scheduling));
     egraph
-        .add_command("run-schedule*".into(), Arc::new(RunExtendedSchedule))
+        .add_command("run-schedule".into(), Arc::new(RunExtendedSchedule))
         .unwrap();
     egraph
 }
